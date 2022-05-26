e3e942883b4b0e2a429418f2ee0ff61e45fbf338
RahulSingh2222 <rs9012854@gmail.com>
2022-05-26 15:21:15 +0530

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,87 @@
+let allMusic = [
+  {
+    name: "Pyaar_Pyaar",
+    artist: "Dino James",
+    img: "music-1",
+    src: "Pyaar_Pyaar_"
+  },
+
+  {
+    name: "Damn",
+    artist: "Raftaar_Ft._KR$NA_",
+    img: "Damn",
+    src: "Damn"
+  },
+
+  {
+    name: "Candy",
+    artist: "Doja_Cat",
+    img: "Candy",
+    src: "Candy"
+  },
+
+  {
+    name: "Need_to_Know",
+    artist: "Doja_Cat",
+    img: "Need_to_Know_",
+    src: "Need_to_Know_"
+  },
+
+  {
+    name: "Woman",
+    artist: "Doja_Cat",
+    img: "Women",
+    src: "Woman"
+  },
+
+  {
+    name: "CLOSE_EYES",
+    artist: "DVRST",
+    img: "CLOSE_EYES",
+    src: "CLOSE_EYES"
+  },
+
+  {
+    name: "Internet_Money",
+    artist: "Don_Toliver,_Gunna_&_Nav_",
+    img: "Internet_Money",
+    src: "Internet_Money"
+  },
+
+  {
+    name: "Ferrari",
+    artist: "James_Hype,_Miggy_Dela_Rosa_",
+    img: "Ferrari",
+    src: "Ferrari"
+  },
+
+  {
+    name: "Honest",
+    artist: "Justin_Bieber",
+    img: "Honest",
+    src: "Honest"
+  },
+
+  {
+    name: "When_You're_Gone",
+    artist: "Shawn_Mendes",
+    img: "when",
+    src: "when"
+  },
+
+  {
+    name: "FRANCHISE",
+    artist: "Travis_Scott_",
+    img: "FRANCHISE",
+    src: "FRANCHISE"
+  },
+
+
+  {
+    name: "Sad",
+    artist: "xxxtentacion",
+    img: "1",
+    src: "sad"
+  },
+
+];
